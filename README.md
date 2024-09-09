bda82dd3cda854f99ee796900e11ecc2f1867e2f
TheFreeMan360 <54360yes@gmail.com>
2024-09-09 21:01:00 +0800

    Updating new files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Web Scraper Framework
+
+用 Clean Architecture 的概念建立了網頁爬蟲的簡易框架
---
+++ scraper_main.py
@@ -0,0 +1,9 @@
+# from web_scraper_framework.use_cases import xx_page_scraper
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
